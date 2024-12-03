6b807854418a2ed48e407be71b172ccb55100e03
Leo o Nardo <leoaoleu@outlook.com>
2024-12-03 10:30:37 -0300

    Criei o arquivo com if else para comparação

---
+++ ClassifierIF.js
@@ -0,0 +1,21 @@
+let heroName = "Lukesky";
+let heroExperience = 4587; 
+
+if (heroExperience < 1000){
+    console.log("O Herói de nome " + heroName + " está no nível de Ferro");
+}else if (heroExperience <= 2000){
+    console.log("O Herói de nome " + heroName + " está no nível de Bronze");
+}else if (heroExperience <= 5000){
+    console.log("O Herói de nome " + heroName + " está no nível de Prata");
+}else if (heroExperience <= 7000){
+    console.log("O Herói de nome " + heroName + " está no nível de Ouro");
+}else if (heroExperience <= 8000){
+    console.log("O Herói de nome " + heroName + " está no nível de Platina");
+}else if (heroExperience <= 9000){
+    console.log("O Herói de nome " + heroName + " está no nível de Ascendente");
+}else if (heroExperience <= 10000){
+    console.log("O Herói de nome " + heroName + " está no nível de Imortal");
+}else {
+    console.log("O Herói de nome " + heroName + " está no nível de Radiante");
+}
+
